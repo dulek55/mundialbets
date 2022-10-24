8a0498ef4245da61b6062e58df9da3f836695a7d
dulek55 <maciekdul95@gmail.com>
2022-10-24 12:55:00 +0200

    Deleted team_name from teams table Deleted TestController.java

--- src/main/java/com/mundial/mundialbets/Entities/TeamEntity.java
+++ src/main/java/com/mundial/mundialbets/Entities/TeamEntity.java
@@ -18,7 +18,6 @@ public class TeamEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
     private String countryName;
     private String countryCode;
 
