881973c48c0e1b51317bf60f9f6c47c0319a0336
DESKTOP-38OVK9Q\oharenda <oharenda@craftware.biz>
2022-10-25 15:19:41 +0200

    add registration and send email confirmation

--- src/main/java/com/mundial/mundialbets/Repositories/UserRepository.java
+++ src/main/java/com/mundial/mundialbets/Repositories/UserRepository.java
@@ -10,4 +10,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<UserEntity, Long> {}
+@Repository
+@Transactional(readOnly=true)
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findUserByEmail(String email);
+    @Transactional
+    @Modifying
+    @Query("UPDATE UserEntity a " +
+            "SET a.enabled = TRUE WHERE a.email = ?1")
+    int enableUser(String email);
+}
--- src/main/java/com/mundial/mundialbets/Services/UserService.java
+++ src/main/java/com/mundial/mundialbets/Services/UserService.java
@@ -1,41 +1,57 @@
 package com.mundial.mundialbets.Services;
 
 import com.mundial.mundialbets.Entities.UserEntity;
+import com.mundial.mundialbets.Registration.Token.ConfirmationTokenEntity;
+import com.mundial.mundialbets.Registration.Token.ConfirmationTokenService;
 import com.mundial.mundialbets.Repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
-public class UserService {
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+    public static final String USER_NOT_FOUND_MSG = "User %s not found";
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ConfirmationTokenService confirmationTokenService;
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findUserByEmail(email).
+                orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                String.format(USER_NOT_FOUND_MSG, email)));
     }
-
-    public UserEntity saveUser(UserEntity userEntity) {
-        return userRepository.save(userEntity);
+    public String signUpUser(UserEntity user) {
+        boolean userExist = userRepository.findUserByEmail(user.getEmail())
+                .isPresent();
+        if(userExist){
+            throw new IllegalStateException("email already taken");
+        }
+        String encodedPassword = bCryptPasswordEncoder
+                .encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        userRepository.save(user);
+        String token = UUID.randomUUID().toString();
+        ConfirmationTokenEntity confirmationToken = new ConfirmationTokenEntity(
+                token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),
+                user
+        );
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
+        return token;
     }
-
-    public UserEntity updateUser(Long id, UserEntity userEntity) throws Exception {
-        userRepository.findById(id).orElseThrow(() -> new Exception("User not found", new Error("USER NOT FOUND")));
-        userEntity.setId(id);
-        return userRepository.save(userEntity);
-    }
-
-    public List<UserEntity> getUsers() {
-        return userRepository.findAll();
-    }
-
-    public UserEntity getUserById(Long id) throws Exception {
-        return userRepository.findById(id)
-                .orElseThrow(() -> new Exception("User id not found : " + id));
-    }
-
-    public void deleteUser(Long id) throws Exception {
-        userRepository.findById(id).orElseThrow(() -> new Exception("User="+ id +" does not exist"));
-        userRepository.deleteById(id);
+    public int enableUser (String email) {
+        return userRepository.enableUser(email);
     }
 }
