953c69e21c63be313bc941c0b033971d44e26b33
dulek55 <maciekdul95@gmail.com>
2022-10-25 21:41:15 +0200

    simple POST and GET to teams table

---
+++ mundialfrontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import AppBar from './components/Appbar';
+import Team from './components/Team';
+
+function App() {
+  return (
+    <div className="App">
+    <AppBar/>  
+    <Team/>
+    </div>
+  );
+}
+
+export default App;
