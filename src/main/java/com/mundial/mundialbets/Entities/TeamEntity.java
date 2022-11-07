9200d197c1730c6bd425f4cb0f93c500c06eb742
dulek55 <maciekdul95@gmail.com>
2022-11-07 14:50:36 +0100

    Hotfix for json references games - teams

--- src/main/java/com/mundial/mundialbets/Entities/TeamEntity.java
+++ src/main/java/com/mundial/mundialbets/Entities/TeamEntity.java
@@ -22,11 +22,11 @@ public class TeamEntity {
     private String countryName;
     private String countryCode;
 
-    @JsonManagedReference(value = "homeTeam_game")
+    @JsonManagedReference(value = "home_team_game")
     @OneToMany(mappedBy = "homeTeam", cascade = CascadeType.ALL)
     private Set<GameEntity> homeGames;
 
-    @JsonManagedReference(value = "awayTeam_game")
+    @JsonManagedReference(value = "away_team_game")
     @OneToMany(mappedBy = "awayTeam", cascade = CascadeType.ALL)
     private Set<GameEntity> awayGames;
 }
